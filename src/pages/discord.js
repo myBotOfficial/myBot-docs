eff0264b9b6498a6029014d7a247bd47fcc45427
ethhaqn <Ethanhorrocks1@outlook.com>
2022-06-25 01:15:36 +0100

    .

--- src/pages/discord.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function Discord() {
-  useEffect(() => {
-    window.location.href = "https://discord.gg/RXATweRCJX";
-  });
-
-  return null;
-}
