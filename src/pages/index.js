e96926d20beaff5325d84d599dfd08754129b323
ethhaqn <Ethanhorrocks1@outlook.com>
2022-06-25 19:09:53 +0100

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,10 @@ export default function Index() {
   return (
     <>
       <Head>
-        <meta name="title" content="myBot Documentation" />
-        <meta name="og:title" content="myBot Documentation" />
-        <meta name="image" content="https://mybot.host/img/gfx/logo.png" />
-        <meta name="og:image" content="https://mybot.host/img/gfx/logo.png" />
+        <meta property="title" content="myBot Documentation" />
+        <meta property="og:title" content="myBot Documentation" />
+        <meta property="image" content="https://mybot.host/img/gfx/logo.png" />
+        <meta property="og:image" content="https://mybot.host/img/gfx/logo.png" />
       </Head>
 
       <Redirect to="/docs/" />
